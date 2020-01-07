c97e39f9393276a53b1ab1b6dd35cc93c4bdcbbe
KOSaber <k.o.saber@hotmail.com>
2020-01-08 00:33:22 +0300

    part2

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -6,14 +6,14 @@ import Fave from './Fave';
 
 class FilmRow extends Component {
   render() {
-      
+       let d = new Date(this.props.date)
     return (
         <div className="film-row">
         <img src={"https://image.tmdb.org/t/p/w780/".concat(this.props.poster)} alt="" />
       
         <div className="film-summary">
           <h1>{this.props.title}</h1>
-          <p>{this.props.date}</p>
+          <p>{d.getFullYear()}</p>
           <Fave /> 
         </div>
       </div>
