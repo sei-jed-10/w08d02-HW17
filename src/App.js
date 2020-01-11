fb96b2bc6b45487aa879d133b9146e5307b06ec1
Sarah <alahmadisr@gmail.com>
2020-01-11 05:33:34 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -9,38 +9,35 @@ class App extends Component {
     super(props)
 
     this.state = {
-      films: TMDB.films,
-      faves: [],
-      current: TMDB.films[3]
+      films: TMDB.films, //set the films state property to the array of films in TMDB.js file.
+      faves: [], //will be used to add and group favorite films.
+      current: TMDB.films[0] //ss an initial state, set the current film to the first one on the list (to display details about it).
     }
+
     this.handleFaveToggle = this.handleFaveToggle.bind(this)
     this.handleDetailsClick = this.handleDetailsClick.bind(this)
-
-
   }
 
   handleFaveToggle(film) { //film will be passed to it.
-    const faves=(this.state.faves).slice()
-    const filmIndex=faves.indexOf(film)
+    const faves = (this.state.faves).slice() //make a copy of the current array of favorite films 'fave'
+    const filmIndex = faves.indexOf(film) //check if the film already exists in the array of favorite films.
 
-    if (filmIndex===-1){
-      console.log("Adding "+film.title+" to faves")
+    if (filmIndex === -1) { //if the film does not exits in the array, push it to the 'faves' array we made above.
+      console.log("Adding " + film.title + " to faves")
       faves.push(film)
     }
-    else{
-      console.log("Removing "+film.title+" from faves")
-      faves.splice(filmIndex,1)
+    else { //if the film already exists in the array, remove it from the list of favorites using Array.splice() method.
+      console.log("Removing " + film.title + " from faves")
+      faves.splice(filmIndex, 1)
     }
-    //need to update the array with the new film object and then set the faves state back to this one.
-    this.setState({faves})
-
-
+    this.setState({ faves }) //update the state of the array of favorites using the copy of the array we updated earlier.
   }
 
-  handleDetailsClick(film) {
-    console.log("Fetching details for "+film.title)
-    this.setState({current: film})
-}
+  handleDetailsClick(film) { //will be used to update the 'current' state
+    console.log("Fetching details for " + film.title)
+    this.setState({ current: film })
+    //change the film details section to the film represented in the film row we just clicked on.
+  }
 
   render() {
 
@@ -48,29 +45,27 @@ class App extends Component {
       <div className="film-library">
         <div className="film-list">
           <h1 className="section-title">FILMS</h1>
+
+          {/* for Display the list of all films by rendering a FilmListing component and sending the states of: films list (films),
+          favorite films list (faves) and both handleFaveToggle() and handleDetailsClick() methods as props to it, either to be used by it or to be passed 
+          down to another component */}
+
           <FilmListing films={this.state.films}
-           faves={this.state.faves}
-            onFaveToggle={(e)=>this.handleFaveToggle(e)}
-              onDetailsToggle={(e)=>this.handleDetailsClick(e)}
-            
-             />
-
-
-
-
-
+            faves={this.state.faves}
+            onFaveToggle={(e) => this.handleFaveToggle(e)}
+            onDetailsToggle={(e) => this.handleDetailsClick(e)} />
         </div>
+
+        {/* for FilmDetails component, pass the state representing the list of films (films) and
+        the state holding the value of the current film to display its details (current), as props to that component. */}
 
         <div className="film-details">
           <h1 className="section-title">DETAILS</h1>
           <FilmDetails films={this.state.films}
-           current={this.state.current}
-            />
+            current={this.state.current} />
         </div>
       </div>
     )
   }
-
 }
-
 export default App;
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -10,11 +10,13 @@ export default function FilmRow(props) {
             <FilmPoster poster={props.poster} />
             {/* add onDetailsToggle() method passed through FilmListing Component to onClick event */}
             <div className="film-summary" onClick={() => props.onDetailsToggle(props)}>
+                <h1>{props.filmTitle}</h1>
                 <p>{props.year}</p>
             </div>
             {/* pass onFaveToggle() as props to the Fave component along with the state (as props) of whether the film is among the list of favorite films */}
-            <Fave onFaveToggle={() => props.onFaveToggle(props)} />
-            isFave={props.isFave}
+            <Fave onFaveToggle={() => props.onFaveToggle(props)}
+            isFave={props.isFave} />
+
         </div>
     )
 }
